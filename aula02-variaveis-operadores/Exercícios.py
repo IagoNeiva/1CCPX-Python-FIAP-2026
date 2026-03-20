9075d7641df951ad418c8a5f807382ca1b9cbde9
IagoNeiva <iagoneiva28@gmail.com>
2026-03-19 22:57:57 -0300

    aula 2 variaveis e operadores

---
+++ aula02-variaveis-operadores/Exercícios.py
@@ -0,0 +1,20 @@
+# Ex 1
+
+nome = input('Digite seu nome: ')
+idade = int(input('Digite sua idade: '))
+print(f'Boas vindas, {nome}, de {idade} anos !!!')
+
+# Ex 2
+nome = input('Digite seu nome: ')
+dia_em_que_nasceu = int(input('Digite o dia em que você nasceu: '))
+mes_em_que_nasceu = input('Escreva o mês em que você nasceu: ')
+ano_em_que_nasceu = int(input('Digite o ano em que você nasceu: '))
+print(f'Olá, {nome}. Você nasceu no dia {dia_em_que_nasceu} de {mes_em_que_nasceu} de {ano_em_que_nasceu}!')
+
+# Ex 3
+nome = input('Digite seu nome: ')
+print('Escolha 2 numeros que vamos soma-los e te dizer o resultado')
+numero_1 = int(input('Digite um numero: '))
+numero_2 = int(input('Digite outro numero: '))
+resultado = numero_1 + numero_2
+print(f'Olá, {nome}. o resultado da sua conta é de {resultado}.')
