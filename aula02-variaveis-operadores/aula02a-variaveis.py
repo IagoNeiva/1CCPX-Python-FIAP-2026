7706c52f30c643f6cc8b6dcb38be9f68b38eaa66
IagoNeiva <iagoneiva28@gmail.com>
2026-03-19 22:28:47 -0300

    aula 2 variaveis e operadores

---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,33 @@
+print("Ola mundo")
+print(7+4)
+print("7+4")
+print("7"+"4") # concatenação de strings
+# Comentários de 1 linha
+'''
+Comentários de
+múltiplas linhas
+'''
+
+
+
+# Variáveis
+nome = "Alexandre" # str
+idade = 26 # int
+peso = 70.2 # float
+
+print(nome, idade, peso)
+print(f"Oiii, {nome}!!!!") # f = formatação, pode colocar o texto + variáveis
+
+# inputs - simulação de forms no cmd
+# input - Pede para o usuário colocar uma informação
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+
+nova_idade = idade + 1
+
+print(nome, idade)
+print(nova_idade)
+
+
